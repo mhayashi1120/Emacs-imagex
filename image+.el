98ad863301440c96b6cf9c5ab3a6018208949d8d
Masahiro Hayashi <mhayashi1120@gmail.com>
2011-11-27 19:21:37 +0900

    version up

--- image+.el
+++ image+.el
@@ -4,7 +4,7 @@
 ;; Keywords: image extensions
 ;; URL: http://github.com/mhayashi1120/Emacs-imagex/raw/master/image+.el
 ;; Emacs: GNU Emacs 22 or later
-;; Version: 0.5.1
+;; Version: 0.5.2
 
 ;; This program is free software; you can redistribute it and/or
 ;; modify it under the terms of the GNU General Public License as
